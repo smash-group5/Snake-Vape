46399f8fac62b7999894a35d96230772d38993d3
smash-group5 <52502358+smash-group5@users.noreply.github.com>
2019-07-05 09:09:15 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function setup(){
+    let canvas = createCanvas(600, 400);
+    myCanvas.parent("smash");
+
+}
+
+function draw(){
+    background(220, 180, 200);
+    ellipse(width/2, height/2, 100, 100);
+    ellipse(width/4, height/2, 50, 50);
+}
